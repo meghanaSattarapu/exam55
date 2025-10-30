86389a4e92a1462d8dbbd72b59c02895158b8251
meghanaSattarapu <163449773+meghanaSattarapu@users.noreply.github.com>
2025-10-30 15:59:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 @app.route('/')
 def form():
     return render_template("form.html")
-@app.route('/submit',method = ['POST'])
+@app.route('/submit',methods = ['POST'])
 def main():
     name = request.form['name']
     date = request.form['date']
-    gender = request.form['gender']
+    gender = request.form('gender')
     return render_template("greetings.html",name = name,date=date,gender = gender)
 if(__name__=="__main__"):
     app.run(host = '0.0.0.0',port = 5000,debug = True)
