6dca6cb5849a9a41303954efca4e693cc24c4b76
meghanaSattarapu <163449773+meghanaSattarapu@users.noreply.github.com>
2025-10-30 15:54:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def form():
 def home():
     name = request.form['name']
     date = request.form['date']
-    gender = request.form('gender')
+    gender = request.form['gender']
     return render_template("greetings.html",name = name,date=date,gender = gender)
 if(__name__=="__main__"):
     app.run(host = '0.0.0.0',port = 5000,debug = True)
