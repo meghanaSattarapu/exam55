77cd6cb17029561a71f151f6594c87b42ee88971
meghanaSattarapu <meghanasattarapu2005@gmail.com>
2025-10-30 14:57:36 +0530

    commit exam

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
+@app.route('/')
+def form():
+    return render_template("form.html")
+@app.route('/submit',method = 'POST')
+def home():
+    name = request.form['name']
+    date = request.form['date']
+    gender = request.form('gender')
+    return render_template("greetings.html",name = name,date=date,gender = gender)
+if(__name__=="__main__"):
+    app.run(debug = True)
